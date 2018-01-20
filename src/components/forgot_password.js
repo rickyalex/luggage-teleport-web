3674c7847eec68e20986bf4a66761210fb30acbc
Luthfir <luthfirrahman04@gmail.com>
2018-01-20 20:22:38 +0700

    adding new page for getting pin to forgot password

---
+++ src/components/forgot_password.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import { userPool } from '../config';
+import { CognitoUser } from "amazon-cognito-identity-js";
+import { withRouter } from 'react-router-dom';
+
+import '../App.css';
+
+class ForgotPassword extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            isLoading: false,
+            error: {
+                message: ''
+            }
+        }
+
+        this.handleConfirmationSubmit = this.handleConfirmationSubmit.bind(this);
+    }
+
+    ForgottenPassword(email) {
+
+        const userData = {
+            Username: email,
+            Pool: userPool
+        }
+        const cognitoUser = new CognitoUser(userData);
+
+        return new Promise((resolve, reject) =>
+            cognitoUser.forgotPassword({
+                onSuccess: function (result) {
+                    console.log(result)
+
+                },
+                onFailure: function (err) {
+                    reject(err)
+                },
+
+                inputVerificationCode: function (data) {
+                    resolve({
+                        cognitoUser: cognitoUser,
+                        thirdArg: this
+                    })
+                }
+            })
+        );
+    }
+
+    async handleConfirmationSubmit(event) {
+        event.preventDefault();
+        this.setState({ isLoading: true })
+        try {
+            await this.ForgottenPassword(this.state.email);
+            this.props.history.push('/confirmnewpassword');
+        } catch (e) {
+            this.setState({
+                error: e,
+                isLoading: false
+            })
+            alert(this.state.error.message);
+        }
+    }
+
+
+    render() {
+
+        const { isLoading } = this.state;
+        return (
+            <div>
+                <div className="bg-image">
+                    <div align="center" style={{ marginTop: '100px' }}>
+
+                        <h1 style={{ color: 'yellow', marginBottom: '2em' }}>Forgot Password</h1>
+                        <form onSubmit={this.handleConfirmationSubmit}>
+                            <div className="form-group">
+                                <input
+                                    className="form-control"
+                                    type="text"
+                                    onChange={e => this.setState({ email: e.target.value })}
+                                    placeholder="Your Email" required />
+                            </div>
+
+                            {
+                                !isLoading ?
+                                    <button
+                                        className="btn btn-lg btn-primary"
+                                        type="submit"
+                                        style={{ width: '160px' }}
+                                    >
+                                        Send Pin
+                                </button>
+                                    :
+                                    <button
+                                        className="btn btn-lg btn-primary"
+                                        type="submit"
+                                        style={{ width: '160px' }}
+                                        disabled={true}
+                                    >
+                                        <i className="fa fa-spinner fa-spin"></i> Sending...
+                                </button>
+                            }
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default withRouter(ForgotPassword);
