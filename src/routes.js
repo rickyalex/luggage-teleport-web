e7c06b72ccecb64a67267d1b1fe9fad4ca652d51
Luthfir <luthfirrahman04@gmail.com>
2018-02-07 09:38:05 +0700

    adding routes for add luggage file and remove unused routes

--- src/routes.js
+++ src/routes.js
@@ -8,10 +8,7 @@ import ForgotPassword from './components/forgot_password';
 import ConfirmNewPassword from './components/confirm_new_password';
 import BookingForm from './components/booking_form';
 import History from './components/history';
-import ATHReview from './components/ath_review';
-import HTAReview from './components/hta_review';
-import HTHReview from './components/hth_review';
-import ATAReview from './components/ata_review';
+import AddLuggage from './components/add_luggage';
 import PaymentMethod from './components/payment_method';
 import ATHFinalReview from './components/ath_final_review';
 import HTAFinalReview from './components/hta_final_review';
@@ -38,15 +35,12 @@ class Routes extends Component {
                         <Route path="/register" component={Register} />
                         <Route path="/booking" component={BookingForm} />
                         <Route path="/history" component={History} />
-                        <Route path="/athreview" component={ATHReview} />
-                        <Route path="/htareview" component={HTAReview} />
-                        <Route path="/hthreview" component={HTHReview} />
-                        <Route path="/atareview" component={ATAReview} />
                         <Route path="/athfinalreview" component={ATHFinalReview} />
                         <Route path="/htafinalreview" component={HTAFinalReview} />
                         <Route path="/hthfinalreview" component={HTHFinalReview} />
                         <Route path="/atafinalreview" component={ATAFinalReview} />
                         <Route path="/payment" component={PaymentMethod} />
+                        <Route path="/addluggage" component={AddLuggage} />
                         <Route path="/verify" component={VerifyAccount} />
                         <Route path="/forgot" component={ForgotPassword} />
                         <Route path="/confirmnewpassword" component={ConfirmNewPassword} />
