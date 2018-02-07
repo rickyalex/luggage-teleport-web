4c9f33296e589726c3b83baaf59bbaf08ba298a5
Luthfir <luthfirrahman04@gmail.com>
2018-02-07 09:42:45 +0700

    adding LuggageData reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,6 +5,7 @@ import payment from './reducer_payment';
 import AirportData from './reducer_airport';
 import AirlineData from './reducer_airline';
 import HotelData from './reducer_hotel';
+import LuggageData from './reducer_add_luggage';
 
 export default combineReducers({
     user,
@@ -12,5 +13,6 @@ export default combineReducers({
     payment,
     AirlineData,
     AirportData,
-    HotelData
+    HotelData,
+    LuggageData
 })
